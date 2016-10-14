b33c140797db55b8114de886e781a2b77eb59c11
mac8695 <Mahesh.Chavan@MyGeorgian.ca>
2016-10-13 21:21:52 -0400

    added github link

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,71 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page */
+/*
+var nodemailer = require('nodemailer');
+
+var router = express.Router();
+//app.use('/sendContactMail', 'router');
+router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
+
+function handleSayHello(req, res) {
+  // Not the movie transporter!
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'chavan.mahesh40@gmail.com', // Your email id
+      pass: '8652587778' // Your password
+    }
+  });
+
+  var text = 'Hello world from \n\n' + req.body.name;
+
+  var mailOptions = {
+    from: 'example@gmail.com>', // sender address
+    to: 'chavan.mahesh40@gmail.com', // list of receivers
+    subject: 'Email Example', // Subject line
+    text: text //, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.json({yo: 'error'});
+    }else{
+      console.log('Message sent: ' + info.response);
+      res.json({yo: info.response});
+    };
+  });
+
+  res.render('index', { title: 'Express' });
+}
+
+*/
+
+/* GET home page
 router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home'});
 });
 
-/* GET about page */
+/* GET about page
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About'});
 });
 
-/* GET projects page */
+/* GET projects page
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects'});
 });
 
-/* GET services page */
+/* GET services page
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'services'});
 });
 
-/* GET about page */
+/* GET about page
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact me'});
 });
-
+*/
 module.exports = router;
